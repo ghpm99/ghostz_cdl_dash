58408d6c523f488d802f15e77daca75cf9a27fb3
Guilherme H <ghpm99@gmail.com>
2023-09-24 22:33:19 -0300

    Feat: Implements fetch next video

--- src/pages/overlay/youtube/[token].tsx
+++ src/pages/overlay/youtube/[token].tsx
@@ -1,35 +1,58 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import YouTube from "react-youtube";
-import { fetchActiveYoutubePlaylistService } from "services/youtube";
+
+import { fetchActiveYoutubePlaylistService, updateStateChangeService } from "services/youtube";
 import styles from "./youtube.module.scss";
+import YouTube from "react-youtube";
+
+interface IYoutubeVideo {
+    id: number;
+    youtube_id: string;
+    title: string;
+    position: number;
+}
+
+const emptyVideoObj: IYoutubeVideo = {
+    id: 0,
+    youtube_id: "",
+    title: "",
+    position: 0,
+};
 
 const OverlayYoutube = () => {
     const router = useRouter();
     const { token } = router.query;
 
-    const [video, setVideo] = useState<{ youtube_id: string; title: string; position: number }>({
-        youtube_id: "",
-        title: "",
-        position: 0,
-    });
-    const [playlist, setPlaylist] = useState([]);
+    const [video, setVideo] = useState<IYoutubeVideo>(emptyVideoObj);
+    const [nextVideo, setNextVideo] = useState<IYoutubeVideo>(emptyVideoObj);
+
+    console.log(video);
+    console.log(nextVideo);
 
     useEffect(() => {
         if (token)
             fetchActiveYoutubePlaylistService(token as string).then((response) => {
-                const playList = response.data.data;
-                setPlaylist(playList);
-                setVideo(playList[0]);
+                const playlist = response.data.data;
+                setVideo(playlist[0]);
+                setNextVideo(playlist[1]);
             });
     }, [token]);
+
+    const updateNextVideo = () => {
+        fetchActiveYoutubePlaylistService(token as string, nextVideo.position).then((response) => {
+            const playlist = response.data.data;
+            setNextVideo(playlist[0]);
+        });
+    };
 
     const opts = {
         width: "1920",
         height: "1080",
+        showinfo: 0,
         playerVars: {
             autoplay: 1,
             origin: "http://localhost:3300",
+            showinfo: 0,
         },
     };
 
@@ -45,9 +68,14 @@ const OverlayYoutube = () => {
     };
 
     const onStateChange = (event) => {
-        console.log(event);
+        updateStateChangeService(token as string, video.id, event.data);
         if (event.data === -1) {
             event.target.playVideo();
+        }
+        if (event.data === 0) {
+            setVideo(nextVideo);
+            event.target.playVideo();
+            updateNextVideo();
         }
     };
 
--- src/services/youtube/index.ts
+++ src/services/youtube/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { API_ENDPOINT, api } from "services";
 
+const YOUTUBE_API_ENDPOINT = "http://192.168.100.5:8300" ?? API_ENDPOINT;
+
 export async function fetchUrlOAuthYoutube() {
     const response = await api.get("/youtube/auth/");
     return response.data;
@@ -27,12 +29,35 @@ export async function updateActiveYoutubePlaylistService(id: number) {
     return response.data;
 }
 
-export async function fetchActiveYoutubePlaylistService(token: string) {
-    const response = await axios.create({ baseURL: API_ENDPOINT }).get("/youtube/playlist/get-active/", {
+export async function fetchActiveYoutubePlaylistService(token: string, position?: number) {
+    const response = await axios.create({ baseURL: YOUTUBE_API_ENDPOINT }).get("/youtube/playlist/get-active/", {
+        params: {
+            position: position,
+        },
         headers: {
             Authorization: "Basic " + token,
         },
     });
+    return {
+        status: response.status,
+        statusText: response.statusText,
+        data: response.data,
+    };
+}
+
+export async function updateStateChangeService(token: string, id: number, state: number) {
+    const response = await axios.create({ baseURL: YOUTUBE_API_ENDPOINT }).post(
+        "/youtube/playlist/set-state/",
+        {
+            id: id,
+            state: state,
+        },
+        {
+            headers: {
+                Authorization: "Basic " + token,
+            },
+        }
+    );
     return {
         status: response.status,
         statusText: response.statusText,
